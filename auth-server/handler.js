a2cdc7368011bbeec2657c97a42be5c84b0739f9
Tony Nicola <tdnicola@gmail.com>
2020-05-25 15:05:06 -0400

    added refreshtokenaccess for aws serverless function.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -3,13 +3,13 @@
 const axios = require('axios');
 const qs = require('qs');
 const config = require('./config');
+const FITBIT_OAUTH_URL = 'https://api.fitbit.com/oauth2/token';
 
 module.exports.getAccessToken = async (event) => {
 	const baseInfo = new Buffer(`${config.id}:${config.secret}`).toString(
 		'base64'
 	);
 
-	const MEETUP_OAUTH_URL = 'https://api.fitbit.com/oauth2/token';
 	const data = {
 		grant_type: 'authorization_code',
 		code: event.pathParameters.code,
@@ -19,7 +19,7 @@ module.exports.getAccessToken = async (event) => {
 
 	const info = await axios({
 		method: 'post',
-		url: MEETUP_OAUTH_URL,
+		url: FITBIT_OAUTH_URL,
 		headers: {
 			Authorization: `Basic ${baseInfo}`,
 			'Content-Type': 'application/x-www-form-urlencoded',
@@ -29,6 +29,44 @@ module.exports.getAccessToken = async (event) => {
 
 	return {
 		statusCode: 200,
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+		},
+		body: JSON.stringify({
+			access_token: info.data.access_token,
+			refresh_token: info.data.refresh_token,
+		}),
+	};
+};
+
+module.exports.refreshAccessToken = async (event) => {
+	const baseInfo = new Buffer(`${config.id}:${config.secret}`).toString(
+		'base64'
+	);
+
+	const data = {
+		grant_type: 'refresh_token',
+		refresh_token: event.pathParameters.code,
+		redirect_uri: 'http://localhost:3000/',
+	};
+
+	const info = await axios({
+		method: 'post',
+		url: FITBIT_OAUTH_URL,
+		headers: {
+			Authorization: `Basic ${baseInfo}`,
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		data: qs.stringify(data),
+	});
+
+	return {
+		statusCode: 200,
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+		},
 		body: JSON.stringify({
 			access_token: info.data.access_token,
 			refresh_token: info.data.refresh_token,
