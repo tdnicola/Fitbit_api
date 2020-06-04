fe4ed1766d1eb46d12004ec21be067b398dfac72
Tony Nicola <tdnicola@gmail.com>
2020-06-04 17:29:43 -0400

    Updates

---
+++ precache-manifest.4f315d20e1ade3074291bc4583b07ec5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d8144b167c48584feb44ade0743ba5",
+    "url": "/Fitbit_api/index.html"
+  },
+  {
+    "revision": "44ea6da4a1f145eeb2a4",
+    "url": "/Fitbit_api/static/css/main.5c11b6fb.chunk.css"
+  },
+  {
+    "revision": "dc141f1c312055bc2cb2",
+    "url": "/Fitbit_api/static/js/2.2fd23f6c.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/Fitbit_api/static/js/2.2fd23f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ea6da4a1f145eeb2a4",
+    "url": "/Fitbit_api/static/js/main.21d553c8.chunk.js"
+  },
+  {
+    "revision": "6cf65dd9762cffce8085",
+    "url": "/Fitbit_api/static/js/runtime-main.9c120be1.js"
+  },
+  {
+    "revision": "5396dbd28a65b7417aae231f8dfe8b10",
+    "url": "/Fitbit_api/static/media/noun_distance_939669.5396dbd2.svg"
+  },
+  {
+    "revision": "f8c444a6d46b97785708ace44b870e6c",
+    "url": "/Fitbit_api/static/media/noun_steps_249968.f8c444a6.svg"
+  },
+  {
+    "revision": "866933309ccc53869c4d1fcc1c626858",
+    "url": "/Fitbit_api/static/media/noun_steps_3105633.86693330.svg"
+  }
+]);
